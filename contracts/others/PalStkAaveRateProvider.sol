17c72950fcce2c03ccab9d1275483f5904c2bdb4
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2022-09-08 14:33:28 +0200

    Update PalStkAaveRateProvider.sol
    made comments to be clarified with Greg.

--- contracts/others/PalStkAaveRateProvider.sol
+++ contracts/others/PalStkAaveRateProvider.sol
@@ -9,6 +9,11 @@
 pragma solidity 0.8.10;
 //SPDX-License-Identifier: MIT
 
+// SEB: Interface matches the Interface defined by Balancer Labs here
+//      https://github.com/balancer-labs/metastable-rate-providers/blob/master/contracts/interfaces/IRateProvider.sol
+//      function name ist the same
+//      visibility is the same
+//      return type is the same
 interface IRateProvider {
     function getRate() external view returns (uint256);
 }
@@ -16,6 +21,10 @@ interface IRateProvider {
 // PalPool Minimal Interface
 interface IPalPool {
 
+    // SEB: This function returns the same type as the IstETH does here
+    //      https://github.com/balancer-labs/metastable-rate-providers/blob/master/contracts/interfaces/IwstETH.sol
+    //      caution: I have not reviewed that `exchangeRateStored` returns the exchangeRate for 1 stkAave
+    //      and not something else or a different unit.
     function exchangeRateStored() external view returns(uint);
     function exchangeRateCurrent() external returns(uint);
 
@@ -46,7 +55,10 @@ contract PalStkAaveRateProvider is IRateProvider {
      */
     function getRate() external view override returns (uint256) {
         uint256 exchangeRate = palStkAavePool.exchangeRateStored();
-
+        
+        // SEB: I understand they use the mantissa_scale for their own calculations
+        //      I am not sure what implications this operation has within the EVM
+        //      But why not leave it out and just return exchangeRate
         return (1e18 * exchangeRate) / MANTISSA_SCALE;
     }
 
